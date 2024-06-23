13c3af84959d5387263a647ac703110b317cf5b4
papatomatoe <papatomatoe.project@gmail.com>
2024-06-23 14:11:25 +0300

    add: page title

--- src/routes/subscribe/+page.svelte
+++ src/routes/subscribe/+page.svelte
@@ -5,6 +5,10 @@
 	let { form }: { form: ActionData } = $props();
 </script>
 
+<svelte:head>
+	<title>Ведьмак | Заявка</title>
+</svelte:head>
+
 <h1 class="v-h">Страница оформления заявки</h1>
 
 <Form {form} />
--- src/routes/subscribe/success/+page.svelte
+++ src/routes/subscribe/success/+page.svelte
@@ -2,6 +2,12 @@
 	import Success from '$lib/components/Success.svelte';
 </script>
 
+<svelte:head>
+	<title>Ведьмак | Заявка отправлена</title>
+</svelte:head>
+
+<h1 class="v-h">Страница подтверждения отправления заявки</h1>
+
 <Success />
 
 <style>
